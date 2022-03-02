10291c7da500fbc952fb060e8899aa5f8e49cc85
pedroobando <pedroobando@hotmail.com>
2022-03-02 08:52:12 -0400

    handleSuccess - promise

--- src/views/public/LoginPage.tsx
+++ src/views/public/LoginPage.tsx
@@ -1,9 +1,8 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import { AUTH_USER } from '../../graphql/dslgql';
 import { fetchgql } from '../../graphql/fetchgql';
 import { useNavigate } from 'react-router-dom';
-// import { useActionGQL } from '../../hooks/useActionGQL';
 
 import { useSEOTitle } from '../../hooks/useSeoPage';
 import { FormLogin } from './components/FormLogin';
@@ -15,33 +14,31 @@ const initialValues: any = {
 
 export const LoginPage = () => {
   const routNavegate = useNavigate();
-
   const { doLogin } = useContext(AuthContext);
-
   useSEOTitle({ subtitle: 'login' });
 
-  //TODO: Arreglar esto manana y transformalo a una funcion asicrona
-  const handleSucces = (value: any): { success: boolean; text: string } => {
+  const handleSuccess = (value: any): Promise<{ success: boolean; text: string }> => {
     const { nickName, password } = value;
-
-    fetchgql(AUTH_USER, { input: { nickName, password } }, '')
-      .then(({ data: { authenticateToken }, errors }) => {
+    let retSuccess = { success: true, text: '' };
+    return fetchgql(AUTH_USER, { input: { nickName, password } }, '')
+      .then(({ data, errors }) => {
         if (errors) {
-          return { success: false, text: errors[0].message };
+          throw new Error(errors[0].message);
         } else {
-          const { token } = authenticateToken;
+          const { token } = data.authenticateToken;
           doLogin(token);
           routNavegate('/');
-          return { success: true, text: '' };
+          return retSuccess;
         }
       })
-      .catch((err) => {});
-    console.log('first');
+      .catch((err) => {
+        return { success: false, text: err.message };
+      });
   };
 
   return (
     <div>
-      <FormLogin initValues={initialValues} handleSucces={handleSucces} />
+      <FormLogin initValues={initialValues} handleSucces={handleSuccess} />
     </div>
   );
 };
